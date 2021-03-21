9a8821415735e581aac60d023262cf22dd0a0e5a
David Bobritsky <dvdbobr@gmail.com>
2021-03-21 12:32:52 +0200

    day1

--- src/w2day1/Components/ChuckNorrisBtn.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class ChuckNorrisBtn extends Component {
-    render() {
-        return (
-            <button>RANDOM JOKE</button>
-        )
-    }
-}
